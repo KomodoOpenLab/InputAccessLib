5871e6fe4444a76498e522266a616cd1017b6b24
Rishabh Jain <rishabhiitd10@gmail.com>
2012-06-14 13:24:58 +0530

    removed unused imports.

--- library/src/ca/idi/tecla/lib/MenuArrayAdapter.java
+++ library/src/ca/idi/tecla/lib/MenuArrayAdapter.java
@@ -6,7 +6,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
