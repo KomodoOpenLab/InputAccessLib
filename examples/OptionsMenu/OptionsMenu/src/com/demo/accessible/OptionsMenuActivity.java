0964c48b8a502e7e000aa2f8e64ac8a32bf5047f
Rishabh Jain <rishabhiitd10@gmail.com>
2012-06-14 16:31:57 +0530

    gave seperate item ids to menu items.

--- examples/OptionsMenu/OptionsMenu/src/com/demo/accessible/OptionsMenuActivity.java
+++ examples/OptionsMenu/OptionsMenu/src/com/demo/accessible/OptionsMenuActivity.java
@@ -23,10 +23,10 @@ public class OptionsMenuActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu){
     	String[] menu_items = getResources().getStringArray(R.array.menu_items);
     	menu.addSubMenu(1, 0, 0, menu_items[0]).setIcon(android.R.drawable.ic_menu_upload);
-    	menu.addSubMenu(1, 0, 0, menu_items[1]);
-    	menu.addSubMenu(1, 0, 0, menu_items[2]).setIcon(android.R.drawable.ic_input_add);
-    	menu.addSubMenu(1, 0, 0, menu_items[3]).setIcon(android.R.drawable.arrow_up_float);
-    	menu.addSubMenu(1, 0, 0, menu_items[4]).setIcon(android.R.drawable.ic_menu_help);
+	menu.addSubMenu(1, 1, 0, menu_items[1]);
+	menu.addSubMenu(1, 2, 0, menu_items[2]).setIcon(android.R.drawable.ic_input_add);
+	menu.addSubMenu(1, 3, 0, menu_items[3]).setIcon(android.R.drawable.arrow_up_float);
+	menu.addSubMenu(1, 4, 0, menu_items[4]).setIcon(android.R.drawable.ic_menu_help);
     	return true;
     }
     
